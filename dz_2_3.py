50540d5f8aa8de2b8e75ed323ba88a6a2a95a7d3
IlyaZhuvagin <71560240+IlyaZhuvagin@users.noreply.github.com>
2020-10-27 18:56:39 +0300

    Add files via upload

---
+++ dz_2_3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([[3, 0], [0, -2]])
+u, s, vh = np.linalg.svd(a)
+plt.arrow(0, 0, *(u[:, 0] * s[0]), color='red')
+plt.arrow(0, 0, *(u[:, 1] * s[1]), color='blue')
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+phi = np.arange(0, 2 * np.pi, 0.01)
+plt.scatter(s[0] * np.cos(phi), s[1] * np.sin(phi), s=0.2)
+plt.show()
+plt.arrow(0, 0, *(vh[:, 0]), color='red')
+plt.arrow(0, 0, *(vh[:, 1]), color='blue')
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
+phi = np.arange(0, 2 * np.pi, 0.001)
+plt.scatter(s[1] * np.cos(phi), s[0] * np.sin(phi), s=0.1)
+plt.show()
---
+++ dz_2_6.py
@@ -0,0 +1,24 @@
+import numpy as np
+from timeit import default_timer as timer
+from numpy.linalg import inv
+
+k = int(input())
+p = int(input())
+diagonal_entiries = np.random.rand(p, p)
+A = np.diag(np.diag(diagonal_entiries))
+A_inver = np.zeros((p, p))
+C = np.eye(k)
+U = np.random.rand(p, k)
+V = np.random.rand(k, p)
+for i in range(p):
+    A_inver[i][i] = 1 / A[i][i]
+start = timer()
+res_1 = A_inver - A_inver @ U @ inv(C + V @ A_inver @ U) @ V @ A_inver
+finish = timer()
+print(res_1)
+print(finish - start)
+for i in range(p):
+    A_inver[i][i] = 1 / A[i][i]
+res_2 = inv(A + U @ C @ V)
+print(res_2)
+print(finish - start)
